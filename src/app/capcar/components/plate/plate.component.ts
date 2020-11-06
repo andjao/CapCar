4b319410af3f38cd6530fb4e55f014aaec2c3c98
andjao <ads.marchi@gmail.com>
2020-11-06 03:15:59 -0300

    add hyphen in string

--- src/app/capcar/components/plate/plate.component.ts
+++ src/app/capcar/components/plate/plate.component.ts
@@ -68,5 +68,10 @@ export class PlateComponent implements OnInit {
         this.plateRequestService.plateResponse.modelo = model.split('chevrolet '.toUpperCase())[1];
         break;
     }
+    if (this.plateRequestService.plateResponse.modelo.split(" ")[0].match(/[0-9]/)) {
+      let words = this.plateRequestService.plateResponse.modelo.split(" ");
+      words[0] = this.plateRequestService.plateResponse.modelo.split(" ")[0].match(/[a-zA-Z]+|[0-9]+/g).join("-");
+      this.plateRequestService.plateResponse.modelo = words.join(" ");
+    }
   }
 }
