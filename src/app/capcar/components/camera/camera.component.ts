a945207054a11f2fdb7de89e97241c38da433363
andjao <ads.marchi@gmail.com>
2020-11-18 23:17:48 -0300

    implement camera 8

--- src/app/capcar/components/camera/camera.component.ts
+++ src/app/capcar/components/camera/camera.component.ts
@@ -117,18 +117,23 @@ export class CameraComponent implements OnInit {
   timerId = null;
 
   addMessage(m) {
-    let regexNational = new RegExp("[a-zA-Z]{3}-[0-9]{4}");
-    let regexMercoSul = new RegExp("[a-zA-Z]{3}[0-9][0-9a-zA-Z][0-9]{2}");
-    if (regexNational.test(m)) {
-      this.closeCam();
-      this.sharedService.mercoSul = false;
-      this.sharedService.plateNational = m.match(regexNational).join("");
-      this.platesComponent.queryPlate();
-    } else if (regexMercoSul.test(m)) {
-      this.closeCam();
-      this.sharedService.mercoSul = true;
-      this.sharedService.plateMercoSul = m.match(regexMercoSul).join("");
-      this.platesComponent.queryPlate();
+    const regexNational = new RegExp("[a-zA-Z]{3}-[0-9]{4}");
+    const regexMercoSul = new RegExp("[a-zA-Z]{3}[0-9][0-9a-zA-Z][0-9]{2}");
+
+    if (!this.sharedService.scamOK) {
+      if (regexNational.test(m)) {
+        this.sharedService.scamOK = true;
+        this.closeCam();
+        this.sharedService.mercoSul = false;
+        this.sharedService.plateNational = m.match(regexNational).join("");
+        this.platesComponent.queryPlate();
+      } else if (regexMercoSul.test(m)) {
+        this.sharedService.scamOK = true;
+        this.closeCam();
+        this.sharedService.mercoSul = true;
+        this.sharedService.plateMercoSul = m.match(regexMercoSul).join("");
+        this.platesComponent.queryPlate();
+      }
     }
   }
 
--- src/app/capcar/components/plates/plates.component.ts
+++ src/app/capcar/components/plates/plates.component.ts
@@ -169,6 +169,7 @@ export class PlatesComponent implements OnInit {
   }
 
   openCam() {
+    this.sharedService.scamOK = true;
     this.sharedService.cameraON = true;
   }
 
